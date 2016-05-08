8d7e2d5dc257bb32fea0f6af5ad731e654f66705
Joy <joyawood@gmail.com>
2016-05-08 15:10:57 -0400

    adding files for data cleaning in r

--- data_prep.r
+++ data_prep.r
@@ -1,26 +1,21 @@
-setwd("/Users/Joy/Downloads")
 library(dplyr)
+
 primaries <- read.csv("primary_results.csv")
 state_names <-read.csv("state_names.csv")
 
 state_names<- state_names %>% 
   select(name, abbreviation) %>% 
   rename(state = name)
-state_names
 
 by_candidate_and_state <- primaries %>% 
   group_by(candidate, state) %>% 
   summarize(sum(votes)) %>% 
   rename(votes_candidate = `sum(votes)`)
 
-by_candidate_and_state
-
 total_votes_state <- primaries %>% 
   group_by(state) %>% 
   summarise(sum(votes)) %>% 
   rename(votes_state = `sum(votes)`)
-
-total_votes_state
 
 merged_data <- inner_join(by_candidate_and_state, total_votes_state, by = "state" ) 
 
@@ -29,6 +24,7 @@ merged_data <- merged_data %>%
 merged_data <- merged_data %>% 
   filter(candidate!= " No Preference" & candidate!= " Uncommitted" )
 merged_data <- left_join(merged_data, state_names)
+
 write.csv(merged_data, file = "final_primary_data.csv")
 
 
